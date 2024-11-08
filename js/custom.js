6a284ac1ae08088656590f98cf56b2acbf5547bd
Almango <hh1830314643@163.com>
2024-11-08 23:15:55 +0800

    Site updated: 2024-11-08 23:15:20

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,5 @@ function scrollToTop() {
 
 // 修改搜索图标
 document.getElementsByClassName("fas fa-search fa-fw")[0].className = "iconfont icon-sousuo"  
+
+document.getElementsByClassName("fas fa-chevron-down").className = "iconfont icon-zhankai"
